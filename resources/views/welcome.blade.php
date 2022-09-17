6d48006c3dfdf4b14aeb3a8b18e7213120bb96f9
Kikic <rickeneke@gmail.com>
2022-09-17 12:59:24 +0800

    Test change in the index file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel MLG Version
                 </div>
 
                 <div class="links">
